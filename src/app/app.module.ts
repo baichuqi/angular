9d71b45bbf890d81bccda779b96aa45e33c6c9df
Haku Soki <cb20079@athome.co.jp>
2021-06-22 17:23:33 +0900

    lesson1-lesson3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        pathMatch: 'full',
+        // loadChildren: () => console.log("")
+        redirectTo: 'list'
+      },
+      // {
+      //   path: 'login',
+      //   pathMatch: 'full',
+      //   // loadChildren: () => import('')
+      // },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
