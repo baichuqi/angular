e285d0160b56fad5c3be6dbcc7248b658c9897f1
Haku Soki <cb20079@athome.co.jp>
2021-06-23 17:05:58 +0900

    serviceの使い方

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,15 +14,19 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
-  heroes = HEROES;
-  constructor() { }
-
+  heroes: Hero[] = [];
+  constructor(private heroService:HeroService, private messageService: MessageService) { }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   selectedHero?:Hero;
   onSelect(hero:Hero): void {
     console.log(hero.name);
     this.selectedHero = hero;
+    this.messageService.add(`heroes component : selected hero id = ${hero.id}`);
   }
 }
